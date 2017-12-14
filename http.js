9cda0706eafcfd9dbfa14ea04d86f3c3d71d51cf
vinodh <vishares@gmail.com>
2017-12-14 17:30:15 +0530

    package json

--- http.js
+++ http.js
@@ -6,7 +6,7 @@ var fs = require("fs");
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 finalResponse = [];
 var app = express();
 
